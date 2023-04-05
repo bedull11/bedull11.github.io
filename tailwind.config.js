0fd07e7e073b14aaebe475bf4eb32fb2d4bb1f34
beduuull <abd.afs39@gmail.com>
2023-04-06 05:11:45 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./**/*.{html,js}"],
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
