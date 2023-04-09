64740b984b0d7f509130c3a4b20a08f3137cb3b9
beduuull <abd.afs39@gmail.com>
2023-04-09 23:21:31 +0700

    add auto type

--- src/script.js
+++ src/script.js
@@ -2,7 +2,22 @@
 feather.replace();
 
 // Nav Scroll ga berfungsi 
-// window.addEventListener('scroll', function() {
-//     var nav = this.document.querySelector('nav');
-//     nav.classList.toggle('bg-white/80', window.scrollY > 0);
-// })
+window.addEventListener('scroll', function() {
+    var nav = this.document.querySelector('nav');
+    nav.classList.toggle('bg-white/80', window.scrollY > 0);
+})
+
+// Hamburger Toggle
+// const hamburger = document.querySelector('#hamburger');
+
+// hamburger.addEventListener('click', function () {
+//     hamburger.classList.toggle('hamburger-active');
+// });
+
+// Auto typed
+var typed = new Typed('.auto-type', {
+    strings : ['UI/UX Design', 'Front End', 'Product Management'],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+});
