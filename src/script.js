5d674cf75190546601c731160c56ad943942568f
beduuull <abd.afs39@gmail.com>
2023-04-11 20:57:18 +0700

    Data AOS

--- src/script.js
+++ src/script.js
@@ -1,11 +1,14 @@
 // Icon feater
 feather.replace();
 
-// Nav Scroll ga berfungsi 
-window.addEventListener('scroll', function() {
-    var nav = this.document.querySelector('nav');
-    nav.classList.toggle('bg-white/80', window.scrollY > 0);
-})
+// AOS
+AOS.init();
+
+// Nav Scroll ga berfungsi
+window.addEventListener("scroll", function () {
+  var nav = this.document.querySelector("nav");
+  nav.classList.toggle("bg-white/80", window.scrollY > 0);
+});
 
 // Hamburger Toggle
 // const hamburger = document.querySelector('#hamburger');
@@ -15,10 +18,17 @@ window.addEventListener('scroll', function() {
 // });
 
 // Auto typed
-var typed = new Typed('.auto-type', {
-    // strings : ['UI/UX Design', 'Front End', 'Product Management'],
-    strings : ['UI/UX Design', 'Product Management'],
-    typeSpeed : 150,
-    backSpeed : 50,
-    loop : true
+var typed = new Typed(".auto-type", {
+  // strings : ['UI/UX Design', 'Front End', 'Product Management'],
+  strings: ["UI/UX Design", "Product Management"],
+  typeSpeed: 150,
+  backSpeed: 50,
+  loop: true,
 });
+
+// preload
+// onload = function () {
+//     let preload = document.getElementById ("preload")
+//     let loading = document.createElement ("div")
+
+// }
